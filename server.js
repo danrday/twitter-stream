3cc6c616d6c81610522717475debf8d0a2197068
Dan Day <danday@gmail.com>
2016-10-08 13:51:44 -0500

    streaming tweets by subject

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 
 var Twitter = require('twitter');
 
-var twitterCreds = require('/private/twitterCreds');
+var twitterCreds = require('./private/twitterCreds');
 
 var client = new Twitter(twitterCreds);
+
+var stream = client.stream('statuses/filter', {track: 'javascript'});
+stream.on('data', function(event) {
+  console.log(event && event.text);
+});
+
+stream.on('error', function(error) {
+  throw error;
+});
