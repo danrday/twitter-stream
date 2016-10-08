064b4a369f710faddd5f617afe96e41cd080f4c8
Dan Day <danday@gmail.com>
2016-10-08 14:20:45 -0500

    setup sockets

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 'use strict'
 
+const express = require('express')
+const { Server } = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = Server(app)
+const io = socketio(server)
+
+const PORT = process.env.PORT || 3000
+
 var Twitter = require('twitter');
 
 var twitterCreds = require('./private/twitterCreds');
@@ -14,3 +24,19 @@ stream.on('data', function(event) {
 stream.on('error', function(error) {
   throw error;
 });
+
+
+//routes
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+//io
+
+io.on('connect', socket => {
+  console.log("socket connected:", socket.id)
+})
+
+server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
